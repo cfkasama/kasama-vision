f3b1b6da32803560876e61905fc09b94a0d7f2c8
akira <kazushim13@gmail.com>
2025-09-02 15:23:11 +0900

    ClientLayout.tsx を更新

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,1 +1,21 @@
+// components/ClientLayout.tsx
+"use client";
 
+import { usePathname } from "next/navigation";
+import RecapchaLoader from "@/components/RecapchaLoader";
+
+const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!;
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname() || "/";
+  const needRecaptcha =
+    pathname.startsWith("/new") ||
+    pathname.startsWith("/posts/"); // コメント投稿ページ
+
+  return (
+    <>
+      {needRecaptcha && <RecapchaLoader siteKey={siteKey} />}
+      {children}
+    </>
+  );
+}
