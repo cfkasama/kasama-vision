93ade108f640ac0f11e299ac80d864bae94e29b3
akira <kazushim13@gmail.com>
2025-09-02 15:36:49 +0900

    Update page.tsx

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -37,7 +37,7 @@ export default async function PostDetail({ params }:{ params:{ id:string }}) {
       </article>
 
       <ReactionBar postId={post.id} likeCount={post.likeCount}  />
-      <CommentList postId={post.id} } />
+      <CommentList postId={post.id}  />
       <ReportButton postId={post.id} />
     </div>
   );
