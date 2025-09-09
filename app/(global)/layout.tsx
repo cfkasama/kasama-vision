cf0ef9969a5134701e857cb63b1a5a2fe859a406
akira <kazushim13@gmail.com>
2025-09-09 13:41:53 +0900

    Update layout.tsx

--- app/(global)/layout.tsx
+++ app/(global)/layout.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from "react";
 import Header from "@/components/Header";
-import "./../styles/globals.css";
-
 export const dynamic = "force-static"; // ここは静的でOK
 export const runtime = "nodejs";
 
@@ -9,7 +7,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <body>
-        <Header municipality={null} />
+        <Header />
         <main className="mx-auto max-w-5xl p-4">{children}</main>
         <footer className="mt-10 border-t">
           <div className="container py-6 text-xs text-gray-500">©cfkasama</div>
