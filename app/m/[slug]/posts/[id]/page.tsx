40a75f1c311cc9620dfa637f7f8b4f2c499050e1
akira <kazushim13@gmail.com>
2025-09-10 13:58:39 +0900

    Update page.tsx

--- app/m/[slug]/posts/[id]/page.tsx
+++ app/m/[slug]/posts/[id]/page.tsx
@@ -1,6 +1,6 @@
 // app/(global)/posts/[id]/page.tsx
 import PostDetail from "@/components/PostDetail";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <PostDetail id={params.id} />;
+export default function Page({ params }: { params: { id: string,slug:string } }) {
+  return <PostDetail id={params.id} slug={params.slug} />;
 }
