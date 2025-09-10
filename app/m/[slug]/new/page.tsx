414225ff68a74107a00b1ac664595cf9e0376759
akira <kazushim13@gmail.com>
2025-09-10 11:44:44 +0900

    Update page.tsx

--- app/m/[slug]/new/page.tsx
+++ app/m/[slug]/new/page.tsx
@@ -11,7 +11,9 @@ export default async function NewPage({
   searchParams: Record<string, string | string[] | undefined>;
 }) {
   const typeFromQuery = typeof searchParams.type === "string" ? searchParams.type : undefined;
-
+  // 存在チェック（名前出したい場合など）
+    const slug = params.slug;
+  
   const municipalities = await prisma.municipality.findMany({
     select: { id: true, name: true, slug: true },
     orderBy: { name: "asc" },
@@ -29,7 +31,7 @@ export default async function NewPage({
     <NewPostClient
       initialType={typeFromQuery}
       municipalities={prioritized}
-      initialMunicipalitySlug="japan" // ← デフォルトを日本に固定
+      initialMunicipalitySlug={slug} 
     />
   );
 }
