10684625e1cc0b2f68bb101009592ae614d007f4
akira <kazushim13@gmail.com>
2025-09-10 10:11:53 +0900

    Update page.tsx

--- app/m/[slug]/page.tsx
+++ app/m/[slug]/page.tsx
@@ -374,7 +374,7 @@ export default async function MunicipalityPage({ params }: { params: { slug: str
           </Link>
         </Card>
       </section>
-      {muni.slug<>'site' ? (
+      {muni.slug!='site' ? (
             <section className="mt-4 grid gap-4">
 <IntentButtons initial={intent} mname={muni.name} mslug={muni.slug}/>
             </section>
