aa2acb9ea6cb5f7c893872bc6bdfc2af35eec88f
akira <kazushim13@gmail.com>
2025-09-12 09:58:43 +0900

    Update ClientLayout.tsx

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -9,7 +9,7 @@ const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!;
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname() || "/";
   const needRecaptcha =
-    pathname.startsWith("/new") ||
+    pathname.startsWith("/new") ||　
     pathname.startsWith("/posts/"); // コメント投稿ページ
 
   return (
