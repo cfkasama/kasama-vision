fe1531a5928d4edb2fcd56839a22ac80f5561cac
akira <kazushim13@gmail.com>
2025-09-18 10:28:51 +0900

    auth-options.ts を更新

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -1,1 +1,13 @@
+// lib/auth-options.ts
+import type { NextAuthOptions } from "next-auth";
+import GitHub from "next-auth/providers/github";
 
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  // 必要に応じて callbacks / session / pages を追記
+};
